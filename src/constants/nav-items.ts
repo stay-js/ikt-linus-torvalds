8917de39b09d213c9bb6340305ef443e33c13d9b
Nagy Zétény <nagyzeteny6@gmail.com>
2023-10-21 17:05:39 +0200

    basic setup

---
+++ src/constants/nav-items.ts
@@ -0,0 +1,18 @@
+export const navItems = [
+  {
+    name: 'Főoldal',
+    path: '/',
+  },
+  {
+    name: 'Élete',
+    path: '/elete',
+  },
+  {
+    name: 'Linux',
+    path: '/linux',
+  },
+  {
+    name: 'Git',
+    path: '/git',
+  },
+] as const;
