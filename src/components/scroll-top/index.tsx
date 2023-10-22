656a01f20c96a674c366e481d1d02462d08811bb
Nagy Zétény <nagyzeteny6@gmail.com>
2023-10-22 12:07:07 +0200

    Create scroll-top button

---
+++ src/components/scroll-top/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { TbArrowUp } from 'react-icons/tb';
+import clsx from 'clsx';
+import style from './scrollTop.module.scss';
+
+export const ScrollTop: React.FC = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const handleScroll = () => (window.scrollY > 300 ? setIsVisible(true) : setIsVisible(false));
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <button
+      className={clsx(style['scroll-top'], isVisible && style.active)}
+      type="button"
+      title="Vissza az oldal tetejére"
+      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+    >
+      <TbArrowUp size={24} color="white" />
+    </button>
+  );
+};
