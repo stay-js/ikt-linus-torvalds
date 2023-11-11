6a871a2c7105acd3c8c20012164ecf78a0e96899
Nagy Zétény <nagyzeteny6@gmail.com>
2023-11-11 19:51:06 +0100

    latest packages, formatting

--- src/components/scroll-top.tsx
+++ src/components/scroll-top.tsx
@@ -32,10 +32,10 @@ export const ScrollTop: React.FC = () => {
         xmlns="http://www.w3.org/2000/svg"
         style={{ color: 'white' }}
       >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M12 5l0 14"></path>
-        <path d="M18 11l-6 -6"></path>
-        <path d="M6 11l6 -6"></path>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 5l0 14" />
+        <path d="M18 11l-6 -6" />
+        <path d="M6 11l6 -6" />
       </svg>
     </button>
   );
